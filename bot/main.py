ba167edb7b78c9bcad88d322d2b081f34ca278c3
Barsuk0 <90799091+Barsuk0@users.noreply.github.com>
2023-09-21 08:06:36 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,13 @@ from aiogram import Bot, Dispatcher
 
 import asyncio
 
-bot = Bot(token='token')
+bot = Bot(token='Done! Congratulations on your new bot. You will find it at t.me/djhadjahda_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+
+Use this token to access the HTTP API:
+6417292366:AAEWkSoI9Rkv7RfNF52v59n48PUAwi_AEfU
+Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+For a description of the Bot API, see this page: https://core.telegram.org/bots/api')
 dp = Dispatcher(bot=bot)
 
 async def main():
